0bfc08a92e5d401ff453b5c73463aa80071bf118
Dmytro Voloshyn <mr.4geym123@gmail.com>
2022-08-17 02:54:19 +0300

    React app with ts

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,24 @@
+export function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nisi labore ipsum perspiciatis repellat ea nostrum. Vitae enim nemo eum blanditiis accusantium, voluptates, hic in ut, sunt a voluptatum itaque.
+    Nemo ad natus facere minus recusandae placeat a quas iste at dolor est odio eos repellat asperiores ex similique quidem labore rerum, perferendis nulla amet. Magni nemo consequatur iste blanditiis?
+    Quis ipsum blanditiis soluta sunt ut delectus nam autem! Aliquid ipsam, quaerat quod, illum porro dicta corrupti nihil cum sint, consequuntur quidem laudantium incidunt quisquam accusantium! Eum, nostrum quis! Ut.
+    Consequuntur sequi dolores ab odio distinctio. Nulla eius non quisquam quod officia. Explicabo voluptate nemo iure modi quasi cumque. Dolorum quia sed assumenda sit odit maiores dolores labore itaque nobis.
+    Placeat, omnis sapiente, eius officiis ad atque, deleniti ipsam ducimus molestias ratione mollitia tenetur assumenda culpa laborum nostrum! Fugit id corporis exercitationem ipsum possimus deserunt sint architecto similique at commodi?
+    Cumque tempora provident recusandae, odit magni in omnis dicta dolorum rem ad doloremque quos aliquid voluptatibus voluptatem dignissimos voluptate quis maiores culpa. Nostrum laudantium sint quae illo amet, cum repellat?
+    Vitae ex dolorum accusantium explicabo debitis quibusdam vel quae! Hic cumque magnam rerum ut atque dolore architecto vel facere nulla eius. Quis, pariatur sunt non hic corporis numquam asperiores nostrum?
+    Officiis qui ducimus neque est corporis vel sequi repellendus a nihil numquam asperiores facilis aliquam quisquam sint quae, ea optio suscipit non magni incidunt vero pariatur laudantium? Explicabo, reprehenderit sunt.
+    Veritatis, tenetur libero officia ratione est beatae iste, natus, possimus inventore adipisci obcaecati ab praesentium repellendus amet excepturi voluptate saepe totam corrupti quis cumque! Illo minus in blanditiis ab expedita.
+    Tempora impedit reiciendis minima nemo vitae praesentium laboriosam molestias beatae excepturi minus quibusdam tenetur ipsa eveniet explicabo maxime assumenda esse, exercitationem aspernatur! Eveniet incidunt doloremque ipsum recusandae voluptatem mollitia corporis.
+    Dignissimos dolorum temporibus, vitae dicta eos odio doloremque ut modi necessitatibus quidem atque dolores blanditiis incidunt aut quas praesentium voluptatem natus, cum laborum libero similique iure. Tenetur dignissimos tempore doloribus.
+    Odit sint cumque exercitationem. Perspiciatis vitae itaque eveniet dolore tenetur. Nam, nesciunt vero in blanditiis iste sed! Aspernatur accusantium voluptatem numquam deserunt minus inventore, vero corrupti ea asperiores molestiae voluptate.
+    Dignissimos nihil sapiente labore, debitis quaerat dolorem voluptate, corrupti enim itaque ut consequuntur in repudiandae aperiam. Quos facilis dolore inventore, veniam aut commodi est enim nam neque cumque, beatae labore?
+    Rem maxime in ipsa assumenda quidem delectus placeat doloribus eveniet unde natus perferendis alias nulla facilis, quae rerum asperiores? Adipisci nostrum ut iure debitis magnam tempora minus quod fuga corrupti?
+    Excepturi ad officiis, vero accusantium adipisci pariatur soluta, repudiandae consequuntur ex impedit, totam eius animi blanditiis molestias. Voluptatibus ipsum ipsam recusandae officia! Iure eaque molestiae illo cupiditate, quasi itaque voluptates.
+    Officia minus molestias odit unde. Fuga eius dolore incidunt itaque in! Voluptas quibusdam saepe corrupti perferendis maiores totam adipisci rem. Nobis ullam velit quia sint ab pariatur maxime aut error?
+    Veritatis exercitationem quasi maxime minima totam repudiandae temporibus itaque voluptas esse laborum. Sunt ab quos quis fuga animi cupiditate maxime alias inventore sint, corrupti facere dolorem voluptatum, porro soluta quas.
+    Consectetur eum incidunt perspiciatis, praesentium commodi facere nam tempore qui ab repellat voluptatibus autem iure, culpa cupiditate, quod nesciunt veniam? Unde optio impedit et debitis ea, voluptatem hic natus praesentium!
+    Sapiente odio corrupti quibusdam hic, quis tempore eos quidem tenetur ea consequuntur velit ipsum beatae minus iste recusandae modi dicta reprehenderit illum accusamus perferendis voluptates unde aspernatur. Voluptatibus, doloremque modi?
+    Dolorum, ducimus porro voluptates dolore quod animi incidunt iste inventore, totam sunt eum qui vel nam accusamus dignissimos quibusdam eveniet fugit voluptas eaque minus temporibus molestias ullam nobis quidem! Voluptates?</div>
+  )
+}
